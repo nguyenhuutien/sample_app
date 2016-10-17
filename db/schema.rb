c4be821c1cbe024b0f5a259b0a78a766dbd1af6d
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-10-17 09:53:15 +0700

    week5

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,9 @@ ActiveRecord::Schema.define(version: 20161008151508) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
