f9a6d9a5ee8c4b326b9d968a36b0ff60f71cdaeb
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-11-03 21:29:09 +0700

    week6-7

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem "bcrypt"
 gem "will_paginate", "3.1.0"
 gem "bootstrap-will_paginate", "0.0.10"
 gem "faker"
+gem "carrierwave", "0.11.2"
+gem "mini_magick", "4.5.1"
+gem "fog"
 
 group :development, :test do
   gem "byebug", platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resources :users
+  resources :account_activations, only: :edit
+  resources :password_resets, except: [:show, :index, :destroy]
+  resources :microposts, only: [:create, :destroy]
 end
